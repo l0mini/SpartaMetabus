147fd74a516c2c668fdd8416865d720cf998890e
l0mini <l0mini@naver.com>
2025-05-02 17:55:19 +0900

    add : 장애물과 장애물 반복생성 구현

--- Assets/Scripts/FlappyPlane/Obstacle.cs
+++ Assets/Scripts/FlappyPlane/Obstacle.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float highPosY = 1f;
+    public float lowPosY = -1f;
 
-    // Update is called once per frame
-    void Update()
+    public float holeSizeMin = 1f;
+    public float holeSizeMax = 1f;
+
+    public Transform topObject;
+    public Transform bottomObject;
+
+    public float widthPadding = 4f;
+
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
     {
-        
+        float holeSize = Random.Range(holeSizeMin, holeSizeMax);
+        float halfHoleSize = holeSize / 2f;
+        topObject.localPosition = new Vector3(0, halfHoleSize);
+        bottomObject.localPosition = new Vector3(0,-halfHoleSize);
+
+        Vector3 placePosition = lastPosition + new Vector3(widthPadding, 0);
+        placePosition.y = Random.Range(lowPosY, highPosY);
+
+        transform.position = placePosition;
+
+        return placePosition;
     }
 }
