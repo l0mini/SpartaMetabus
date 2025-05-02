30620ac2a8c70523de444e6a4a878deeda203344
l0mini <l0mini@naver.com>
2025-05-02 17:41:08 +0900

    add : 비행기 및 지형생성, 비행기 움직임 구현

--- Assets/Scripts/FlappyPlane/Flappy.cs
+++ Assets/Scripts/FlappyPlane/Flappy.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Flappy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator = null;
+    Rigidbody2D rb = null;
+
+    public float jumpForce = 6f;
+    public float forwardSpeed = 3f;
+    public bool isDead = false;
+    float deathCooldown = 0f;
+
+    bool isJump = false;
+
+    public bool godMode = false;
+
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponentInChildren<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+        {
+            if(deathCooldown <= 0f)
+            {
+                if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+                {
+                   
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+                {
+                    isJump = true;
+                }
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if(isDead)
+            return;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = forwardSpeed;
         
+        if(isJump)
+        {
+            velocity.y += jumpForce;
+            isJump = false;
+        }
+
+        rb.velocity = velocity;
+
+        float angle = Mathf.Clamp((rb.velocity.y * 10f), -90, 90);
+        transform.rotation = Quaternion.Euler(0,0,angle);
+    }
+
+    public void OnCollisionEnter2D(Collision collision)
+    {
+        if (godMode)
+            return;
+
+        if (isDead)
+            return;
+
+        animator.SetInteger("IsDie", 1);
+        isDead = true;
+        deathCooldown = 1f;
     }
 }
