8f220c6c7cfe66d749d5634bedd7e92219e4cf31
l0mini <l0mini@naver.com>
2025-05-02 18:18:07 +0900

    add : 배경과 장애물 반복생성

--- Assets/Scripts/FlappyPlane/BgLooper.cs
+++ Assets/Scripts/FlappyPlane/BgLooper.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BgLooper : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int obstacleCount = 0;
+    public Vector3 obstacleLastPosition = Vector3.zero;
+
+    public int numBgCount = 5;
+
     void Start()
     {
-        
+        Obstacle[] obstacles = GameObject.FindObjectsOfType<Obstacle>();
+        obstacleLastPosition = obstacles[0].transform.position;
+        obstacleCount = obstacles.Length;
+
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            obstacleLastPosition = obstacles[i].SetRandomPlace(
+                obstacleLastPosition, obstacleCount);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Obstacle obstacle = collision.GetComponent<Obstacle>();
+        if (obstacle)
+        {
+            obstacleLastPosition = obstacle.SetRandomPlace(
+                obstacleLastPosition, obstacleCount);
+        }
+
+        if (collision.CompareTag("BackGround"))
+        {
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * numBgCount;
+            collision.transform.position = pos;
+            return;
+        }
     }
 }
